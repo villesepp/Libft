2f38ce19f5366db3bfa91bcf415d90d0aa4327fe
Ville Seppanen <vseppane@c3r5p6.hive.fi>
2024-10-19 13:21:00 +0300

    atoll and isspace

---
+++ ft_atoll.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vseppane <vseppane@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 13:18:11 by vseppane          #+#    #+#             */
+/*   Updated: 2024/10/19 13:18:25 by vseppane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+long long	ft_atoll(const char *str)
+{
+	long long	number;
+	int			sign;
+	int			i;
+
+	number = 0;
+	sign = 1;
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		number = number * 10 + str[i] - '0';
+		i++;
+	}
+	return (number * sign);
+}
